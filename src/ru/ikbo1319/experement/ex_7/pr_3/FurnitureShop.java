59e45ae0fd3b0642156a9ab377eb087f491ddc00
haniko135 <nastya.izotina@mail.ru>
2020-09-29 20:53:35 +0300

    full ex_7

--- src/ru/ikbo1319/experement/ex_7/pr_3/FurnitureShop.java
+++ src/ru/ikbo1319/experement/ex_7/pr_3/FurnitureShop.java
@@ -1,20 +1,30 @@
 package ru.ikbo1319.experement.ex_7.pr_3;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FurnitureShop {
-    private double price;
-    private Furniture[] furniture;
+    private ArrayList<Furniture>furniture;
     private String[] categories;
+    Scanner scanner = new Scanner(System.in);
 
-    public FurnitureShop(int n){
-        furniture = new Furniture[n];
+    public FurnitureShop(){
+        furniture = new ArrayList<Furniture>();
+        //categoriesList();
     }
-    public void categoriesList(){
+
+    /*public void categoriesList(){
         categories = new String[3];
-    }
+        categories[1] = "Shelfs";
+        categories[2] = "Tables";
+        categories[3] = "Chairs";
+    }*/
 
-    public static void main(){
-
+    public void setFurniture(int n){
+        for(int i = 0; i < n; i += 3){
+            furniture.add(i, new Table(scanner.nextLine(),scanner.nextLine(),scanner.nextLine(), scanner.nextDouble(),scanner.nextLine(), scanner.nextDouble()));
+            furniture.add(i+1, new Shelf(scanner.nextLine(),scanner.nextLine(),scanner.nextLine(), scanner.nextDouble(),scanner.nextLine()));
+            furniture.add(i+2, new Chair(scanner.nextLine(),scanner.nextLine(),scanner.nextLine(), scanner.nextDouble(),scanner.nextLine(), scanner.nextDouble()));
+        }
     }
 }
--- src/ru/ikbo1319/experement/ex_7/pr_3/Person.java
+++ src/ru/ikbo1319/experement/ex_7/pr_3/Person.java
@@ -1,17 +1,18 @@
 package ru.ikbo1319.experement.ex_7.pr_3;
 
-public class Person {
-    private String[] basket;
-    private double index = 0;
+import java.util.ArrayList;
 
-    public Person(String[] basket) {
-        this.basket = basket;
+
+public class Person{
+    private ArrayList<FurnitureShop> basket;
+    private int index = 0;
+
+    public Person() {
+        basket = new ArrayList<FurnitureShop>();
     }
 
-    public void addBasket(int n, String furn){
-        for(int i = 0; i < n; i++){
-            basket[i] = new String(furn);
-        }
-        index += n;
+    public void addBasket(FurnitureShop element){
+        basket.add(index, element);
+        index += 1;
     }
 }
